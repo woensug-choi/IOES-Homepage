4c21f1edb39e12a25436366f316260a97b27f5e3
Woen-Sug Choi <woensug.choi@gmail.com>
2025-06-30 18:14:25 +0900

    new images

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -43,7 +43,7 @@ const ExploreCard = ({
         <div className="absolute bottom-0 p-8 max-sm:py-4 justify-start w-full flex-col bg-[rgba(0,0,0,0.5)] rounded-b-[24px] ">
           <div className="flex grid-cols-2 justify-between">
             <p
-              className={`${styles.flexCenter} h-[60px] max-sm:h-[40px] rounded-[15px] glassmorphism mb-[16px] max-sm:mb-2 text-[18px] leading-[20px] px-4`}
+              className={`${styles.flexCenter} h-[50px] max-sm:h-[40px] rounded-[15px] glassmorphism mb-[10px] max-sm:mb-2 text-[18px] leading-[20px] px-4`}
             >
               {category === "Research" ? (
                 <span className="font-bold text-emerald-400">
@@ -60,10 +60,10 @@ const ExploreCard = ({
               )}
             </p>
           </div>
-          <h2 className="mt-[24px] max-sm:mt-[12px] font-bold sm:text-[32px] text-[24px] text-white ">
+          <h2 className="mt-[24px] max-sm:mt-[12px] font-bold  max-sm:text-[20px] text-[24px] text-white ">
             {t(title)}
           </h2>
-          <p className="font-normal text-[16px] leading-[20px] text-white/80">
+          <p className="font-normal max-sm:text-[12px] text-[16px] leading-[20px] text-white/80">
             {t(tags)}
           </p>
           <p className="font-normal mt-5 text-[16px] leading-[20px] uppercase">
