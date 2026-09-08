278b4507816ead840c4af80bb89c280c8a22611c
woensug-choi <woensug.choi@gmail.com>
2026-09-08 13:53:13 +0900

    fix notch color

--- app/head.js
+++ app/head.js
@@ -2,6 +2,7 @@ const Head = () => (
   <>
     <title>IOES Lab.</title>
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#1A232E" />
     <link rel="icon" href="/favicon.ico" />
   </>
 );
